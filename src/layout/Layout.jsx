6dca92e2d9d6a48e567324f9571b9f662ded44c3
“Flavia <flaviferri90@gmail.com>
2024-08-20 17:17:32 +0200

    Completed detail component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar/Navbar";
+/* import Navbar from "../components/navbar/Navbar"; */
+import Detail from "../pages/Detail";
 
 
 
 const Layout = () => {
     return (
         <>
-            <Navbar />
-            <main>
+            <Detail />
+            <main className="font-jaldi">
                 <Outlet /> 
-
+            
             </main>
-            <Footer />
+
 
         </>
     )
