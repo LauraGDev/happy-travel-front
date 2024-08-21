61b6d32256dd4f2ffaf89ba413f4a6c4364a5acd
indiakka <78439130+indiakka@user.noreply.github.com>
2024-08-21 12:12:13 +0200

    desktop view of header and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ import Navbar from "./navbar/Navbar";
 const Header = ({ isLogged }) => {
   return (
     <div className="flex flex-col min-h-screen justify-start">
-      <header className="w-full bg-white py-4 flex justify-between items-center px-8">
-        <div className="flex items-center">
+      <header className="w-full bg-white py-4 flex justify-center lg:justify-between lg:items-center px-8">
+        <div className="flex items-center justify-center">
           <img src="/Assets/Logo.svg" alt="logo" className="h-16" />
         </div>
         <div className="hidden lg:flex mt-7">
@@ -13,7 +13,7 @@ const Header = ({ isLogged }) => {
         </div>
       </header>
       <div className="w-full flex justify-center">
-        <div className="w-[95%] border-b-2 border-blue "></div>
+        <div className=" lg:w-[95%] lg:border-b-2 lg:border-blue "></div>
       </div>
 
       <div className="lg:hidden fixed bottom-4 w-full h-10 flex justify-center items-center">
