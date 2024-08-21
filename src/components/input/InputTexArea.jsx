e7acbb769ed48852c597fef438ece215a026e5b4
Betty Lopez <elisabetlopezpons@gmail.com>
2024-08-21 16:50:25 +0200

    Adjust mediaqueries desktop

--- src/components/input/InputTexArea.jsx
+++ src/components/input/InputTexArea.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const InputTexArea = ({description, title}) => {
+  const textareaRef = useRef(null);
+
+  const adjustHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    adjustHeight();
+  }, []);
+
   return (
-    <div className="py-1">
+    <div className="py-1 pl-1">
         <h2 className="font-jaldi text-blue text-[1.1rem] font-bold ml-2">{title}</h2>
-        <textarea name="descripción" id="" className="rounded-[1.2rem] focus:ring-4 text-base font-jaldi font-[400] py-[1.2rem] pl-[1rem] pr-[1rem] w-[22rem] shadow-[inset_0_4px_4px_-0px_rgba(0,0,0,0.2)] bg-yellow h-[90%] text-blue">{description}</textarea>
+        <textarea name="descripción" id="" className="rounded-[1.2rem] focus:ring-4 text-base font-jaldi font-[400] py-[1.2rem] pl-[1rem] pr-[1rem] lg:w-[95%] w-[18rem] shadow-[inset_0_4px_4px_-0px_rgba(0,0,0,0.2)] bg-yellow lg:h-[100%] 
+       h-[auto] text-blue" ref={textareaRef} onInput={adjustHeight}>{description}</textarea>
     </div>
   )
 }
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -32,12 +32,12 @@ const Edit = () => {
 
   return (
     <div className="flex justify-center w-[auto] py-2">
-      <section className="w-[44rem] rounded-[1.2rem] bg-white border-4 border-yellow gap-2 py-[1.2rem] px-[1.5rem]">
+      <section className="lg:w-[45%] w-[22rem] rounded-[1.2rem] bg-white border-4 border-yellow gap-2 py-[1.2rem] px-[1.5rem]">
       <form action="" onSubmit={handleSubmit} >
       <h1 className="text-pink font-jaldi font-bold text-center text-[1.4rem] py-1">Editar destino</h1>
       <hr className="w-[100%] size-2 border-pink"/>
-        <section className="flex flex-col lg:flex-row pt-3 justify-between gap-[1rem]">
-          <section className=" w-[42%] items-start space-y-6">
+        <section className="flex flex-col lg:grid lg:grid-cols-[repeat(2,50%)] lg:grid-rows-[repeat(3,auto)] pt-3 justify-center gap-[1rem]">
+          <section className="w-[100%] lg:w-[88%] lg:row-[1_/_3] lg:col-[1_/_span_1] lg:items-start items-center lg:justify-items-between space-y-5">
               <Input 
                 title="Título" 
                 type="text"
@@ -55,13 +55,16 @@ const Edit = () => {
               <InputImg 
               title="Imagen"
               placeholder="Escoge una imagen..."/>
-              <div className="flex flex-row py-1 pt-3 gap-[0.1rem]">
+              
+          </section>
+          <InputTexArea 
+          title="¿Por qué quieres viajar allí?" 
+          description="At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies. At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies. At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies." 
+          className="lg:row-[span_4] lg:w-[90%]" />
+          <div className="flex flex-row justify-between lg:justify-start py-1 lg:row-start-[span_3] lg:col-[1_/_span_1] gap-[1rem]">
                 <Button className="bg-green" text="Aceptar" type="submit"/>
                 <Button className="bg-pink" text="Cancelar"/>
-              </div>
-          </section>
-          <InputTexArea title="¿Por qué quieres viajar allí?" description="At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies." />
-          
+          </div>
         </section>
         </form>
       </section>
