b8ae9b63d1976e52a95306dbd2bf494ee5753470
Laura Gil <laura.gilcara@gmail.com>
2024-08-22 15:58:00 +0200

    fetch tha destinations data from the api

--- src/components/home/CardsContainer.jsx
+++ src/components/home/CardsContainer.jsx
@@ -6,12 +6,14 @@ const CardsContainer = () => {
     const itemsPerPage = 8;
     const [actualPage, setActualPage] = useState(1);
     const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+    const [destinations, setDestinations] = useState([])
 
     const updatePageNumber = (page) => {
         setActualPage(page);
     };
 
     useEffect(() => {
+        fetchDestinations();
         const handleResize = () => {
             setIsLargeScreen(window.innerWidth >= 768);
         };
@@ -19,18 +21,20 @@ const CardsContainer = () => {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
     
-
-    //ESTO SE BORRA ES PARA PROBAR COMO SE VEN y aquí iría el fetch creo
-    let destinations = []
-    for(let i = 0; i < 18; i++) {
-        if(i < 8) {
-            destinations.push(["Assets/foto-prueba-BORRAR.png", "Islas Azores", "Portugal"]);
-        } else if ( i > 7 && i < 16) {
-            destinations.push(["Assets/foto-prueba-BORRAR-2.jpg", "Marina D'Or", "Españita"]);
-        } else {
-            destinations.push(["Assets/foto-prueba-BORRAR-3.jpg", "Phuket", "Tailandia"]);
+    const fetchDestinations = async () => {
+        try {
+            const response = await fetch('http://localhost:4001/destinations', {
+                method: 'GET'
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch destinations');
+            }
+            const data = await response.json();
+            setDestinations(data);
+        } catch (error) {
+            console.error('Error fetching destinations:', error);
         }
-    }
+    };
 
     const lastElementIndex = actualPage * itemsPerPage;
     const firstElementIndex = lastElementIndex - itemsPerPage;
@@ -38,14 +42,14 @@ const CardsContainer = () => {
 
     return (
       <section className="flex flex-col">
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 mx-12 my-8 lg:mx-[4.375rem] z-[-1] relative">
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 mx-8 my-8 lg:mx-[4.375rem] z-[-1] relative">
           {actualItems?.map((des, index) => (
             <DestinationCard
               key={index}
-              photo={des[0]}
-              destination={des[1]}
-              country={des[2]}
-              createdBy={3}
+              photo={des.image}
+              name={des.name}
+              country={des.country}
+              createdBy={des.user_id}
               actualUser={2}
               isLoggedIn={false}
             />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import CardsContainer from '../components/home/CardsContainer'
 const Home = () => {
   return (
     <main className="max-w-[118.75rem] my-0 mx-auto pb-6">
-      <CardsContainer className="z-[-9999]" />{" "}
+      <CardsContainer className="z-[-9999]" />
     </main>
   );
 }
