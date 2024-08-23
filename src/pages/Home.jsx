ddbfcc0e8171e816dc718627fd17424ef0c6492c
“Flavia <flaviferri90@gmail.com>
2024-08-23 08:52:12 +0200

    Pop up meriaQuerys working on

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import Button from '../components/buttons/Button'
 import DestinationCard from '../components/home/DestinationCard'
 import SearchBar from '../components/navbar/SearchBar'
 import PopUp from '../components/popUp/popUp'
+import Navbar from '../components/navbar/Navbar'
 
 
 const Home = () => {
   return (
     <div>
+
       {/* <Button text="Aceptar" className="bg-green"/>
       <Button text="Cancelar" className="bg-pink"/> */}
+      <Navbar/>
       <DestinationCard/>
       <SearchBar/>
       <PopUp/>
