9c9ad4b7a8c9aee7cda839801a66314342f7f2a1
Laura Gil <laura.gilcara@gmail.com>
2024-08-28 12:56:25 +0200

    make the popup a portal to make it appear in front of everything

--- src/components/popUp/popUp.jsx
+++ src/components/popUp/popUp.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../buttons/Button';
+import ReactDOM from 'react-dom';
 
 const Popup = ({ isPopupOpen, closePopup, message, onConfirm }) => {
 
@@ -20,34 +21,40 @@ const Popup = ({ isPopupOpen, closePopup, message, onConfirm }) => {
     }
   }, [isPopupOpen]);
 
-  return (
-    isPopupOpen && (
-      <div className="absolute w-full h-screen bg-[#000000a8] top-0 left-0 z-[9999]">
-        <div className="ventana-Popup">
-          <div className="bg-yellow h-[10.5rem] w-[21.6875rem] text-center rounded-[10%] fixed inset-0 m-auto">
-            <p className="text-blue pt-[1.30rem] text-[1.25rem]">{message}</p>
-            <div className="flex justify-center gap-[0.625rem] mt-[1.25rem]">
-              <Button
-                text="Aceptar"
-                type="button"
-                className="bg-green"
-                onClick={(e) => {
-                  if (onConfirm) onConfirm(e);
-                  closePopup();
-                }}
-              />
-              <Button
-                text="Cancelar"
-                className="bg-pink"
-                onClick={closePopup}
-                type='button'
-              />
-            </div>
+  
+  
+  if (!isPopupOpen) return null;
+
+  const popupContent = (
+    <div className="absolute w-full h-screen bg-[#000000a8] top-0 left-0 z-[1000]">
+      <div className="ventana-Popup">
+        <div className="bg-yellow h-[10.5rem] w-[21.6875rem] lg:w-[29.375rem] lg:h-[12.5rem] text-center rounded-[10%] fixed inset-0 m-auto flex flex-col align-middle justify-center ">
+          <p className="text-blue pt-[1.30rem] text-[1.25rem]">{message}</p>
+          <div className="flex justify-center gap-[0.625rem] mt-[1.25rem] pb-4">
+            <Button
+              text="Aceptar"
+              type="button"
+              className="bg-green"
+              onClick={(e) => {
+                if (onConfirm) onConfirm(e);
+                closePopup();
+              }}
+            />
+            <Button
+              text="Cancelar"
+              className="bg-pink"
+              onClick={closePopup}
+              type='button'
+            />
           </div>
         </div>
       </div>
-    )
+    </div>
   );
+
+
+  return ReactDOM.createPortal(popupContent, document.body);
+
 };
 
 Popup.propTypes = {
